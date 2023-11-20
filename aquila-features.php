be7b8a4705e37dd1824dc03cfe69f7b609e7a0eb
Fahim Mahmud <41551321+mdfahimmahmud@users.noreply.github.com>
2023-11-20 22:16:13 +0600

    second commit

---
+++ aquila-features.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Aquila Features Plugin
+ *
+ * @package aquila-features
+ * @author  Fahim Mahmud
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Aquila Features
+ * Plugin URI:        https://codeytek.com/aquila-features/
+ * Description:       Adds Gutenberg Blocks.
+ * Version:           1.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Fahim Mahmud
+ * Author URI:        https://codeytek.com/about/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       aquila-features
+ * Domain Path:       /languages
+ */
+
+/**
+ * Bootstrap the plugin.
+ */
+require_once 'vendor/autoload.php';
+require_once untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/inc/custom-functions.php';
+
+use AquilaFeatures\Plugin;
+
+if ( class_exists( 'AquilaFeatures\Plugin' ) ) {
+	$the_plugin = new Plugin();
+}
+
+register_activation_hook( __FILE__, [ $the_plugin, 'activate' ] );
+register_deactivation_hook( __FILE__, [ $the_plugin, 'deactivate' ] );
